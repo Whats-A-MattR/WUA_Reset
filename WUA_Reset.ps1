00c811db2fd2edcfad3eb160de489d1980266dbb
Matthew Russell <matthew.russell@adit.net.au>
2019-11-22 14:29:18 +1000

    Create WUA_Reset.ps1

---
+++ WUA_Reset.ps1
@@ -0,0 +1,77 @@
+<#
+.Synopsis
+
+Following process outlined in https://docs.microsoft.com/en-us/windows/deployment/update/windows-update-resources
+
+Created script for ease of use
+
+
+#>
+
+<#
+.Author
+
+Matthew Russell
+https://github.com/ScriptedAdventures
+https://www.scriptedadventures.net/
+
+#>
+Push-Location -Path "$ENV:WINDIR\SYSTEM32"
+
+$qmgr = "$ENV:ALLUSERSPROFILE\Application Data\Microsoft\Network\Downloader\"
+
+Get-Service -Name BITS | Stop-Service
+Get-Service -Name wuauserv | Stop-Service
+
+Get-ChildItem -Path $qmgr | Where {($_.FullName -like "*qmgr*") -and ($_.FullName -like "*.dat")} | Remove-Item -Force
+
+Rename-Item -Path "$ENV:SYSTEMROOT\SoftwareDistribution\Datastore" -NewName "$ENV:SYSTEMROOT\SoftwareDistribution\Datastore.bak"
+Rename-Item -Path "$ENV:SYSTEMROOT\SoftwareDistribution\Download" -NewName "$ENV:SYSTEMROOT\SoftwareDistribution\Download.bak"
+Rename-Item -Path "$ENV:SYSTEM32\catroot2" -NewName "$ENV:SYSTEM32\catroot.bak"
+
+sc.exe sdset bits D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;AU)(A;;CCLCSWRPWPDTLOCRRC;;;PU)
+sc.exe sdset wuauserv D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;AU)(A;;CCLCSWRPWPDTLOCRRC;;;PU)
+
+regsvr32.exe atl.dll
+regsvr32.exe urlmon.dll
+regsvr32.exe mshtml.dll
+regsvr32.exe shdocvw.dll
+regsvr32.exe browseui.dll
+regsvr32.exe jscript.dll
+regsvr32.exe vbscript.dll
+regsvr32.exe scrrun.dll
+regsvr32.exe msxml.dll
+regsvr32.exe msxml3.dll
+regsvr32.exe msxml6.dll
+regsvr32.exe actxprxy.dll
+regsvr32.exe softpub.dll
+regsvr32.exe wintrust.dll
+regsvr32.exe dssenh.dll
+regsvr32.exe rsaenh.dll
+regsvr32.exe gpkcsp.dll
+regsvr32.exe sccbase.dll
+regsvr32.exe slbcsp.dll
+regsvr32.exe cryptdlg.dll
+regsvr32.exe oleaut32.dll
+regsvr32.exe ole32.dll
+regsvr32.exe shell32.dll
+regsvr32.exe initpki.dll
+regsvr32.exe wuapi.dll
+regsvr32.exe wuaueng.dll
+regsvr32.exe wuaueng1.dll
+regsvr32.exe wucltui.dll
+regsvr32.exe wups.dll
+regsvr32.exe wups2.dll
+regsvr32.exe wuweb.dll
+regsvr32.exe qmgr.dll
+regsvr32.exe qmgrprxy.dll
+regsvr32.exe wucltux.dll
+regsvr32.exe muweb.dll
+regsvr32.exe wuwebv.dll
+
+netsh winsock reset
+
+Get-Service -Name BITS | Start-Service
+Get-Service -Name wuauserv | Start-Service
+
+bitsadmin.exe /reset /allusers
